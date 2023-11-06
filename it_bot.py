131fd037f252790e2c8f29aafe03ba51f3845b39
Toktorov <ktoktorov2005@gmail.com>
2023-11-06 19:35:40 +0600

    mailing bo

--- it_bot.py
+++ it_bot.py
@@ -1,10 +1,27 @@
 from aiogram import Bot, Dispatcher, types, executor
+from aiogram.dispatcher.storage import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
 from config import token 
 from logging import basicConfig, INFO
+import sqlite3, time
 
 bot = Bot(token=token)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 basicConfig(level=INFO)
+
+connection = sqlite3.connect('users.db')
+cursor = connection.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER,
+    username VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    created VARCHAR(255)
+);
+""")
 
 start_buttons = [
     types.KeyboardButton('О нас'),
@@ -14,8 +31,19 @@ start_buttons = [
 ]
 start_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).add(*start_buttons)
 
+"Нужно реализовать функцию записи пользователя в нашу базу данных users при работе с коммандой /start. Если пользователь есть в базе, то его не записываете!"
+
 @dp.message_handler(commands='start')
 async def start(message:types.Message):
+    cursor.execute(f"SELECT id FROM users WHERE id = {message.from_user.id};")
+    result = cursor.fetchall()
+    if result == []:
+        cursor.execute(f"""INSERT INTO users VALUES(
+            {message.from_user.id}, '{message.from_user.username}',
+            '{message.from_user.first_name}', '{message.from_user.last_name}',
+            '{time.ctime()}'
+        );""")
+        cursor.connection.commit()
     await message.answer(f"Здравствуйте {message.from_user.full_name}, добро пожаловать в Geeks!", reply_markup=start_keyboard)
     print(message)
 
@@ -71,5 +99,32 @@ async def uxui(message:types.Message):
 @dp.message_handler(text="Назад")
 async def cancell(message:types.Message):
     await start(message)
+
+class MailingState(StatesGroup):
+    message = State()
+
+@dp.message_handler(text="Рассылка")
+async def mailing(message:types.Message):
+    if message.from_user.id in [731982105]:
+        await message.reply("Введите свой текст для рассылки")
+        await MailingState.message.set()
+    else:
+        await message.reply("У вас нет прав на рассылку")
+
+@dp.message_handler(state=MailingState.message)
+async def send_mailing(message:types.Message, state:FSMContext):
+    await message.answer("Начинаю рассылку...")
+    cursor.execute("SELECT id FROM users;")
+    users_id = cursor.fetchall()
+    print(users_id)
+    for i in users_id:
+        print(i[0])
+        await bot.send_message(i[0], message.text)
+    await message.answer("Рассылка окончена")
+    await state.finish()
+
+@dp.message_handler()
+async def not_found(message:types.Message):
+    await message.reply("Я вас не понял, введите /help")
 
 executor.start_polling(dp)
